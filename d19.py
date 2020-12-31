cd46d422e7052f08a4a152329f2b2de4923349b6
Christian Jauvin <cjauvin@gmail.com>
2020-12-31 10:19:00 -0500

    D19

--- d19.py
+++ d19.py
@@ -1,56 +1,90 @@
 import re
+import sys
 
 s = """
-0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: "a"
-5: "b"
+0: 8 11
+1: "a"
+2: 1 24 | 14 4
+3: 5 14 | 16 1
+4: 1 1
+5: 1 14 | 15 1
+6: 14 14 | 1 14
+7: 14 5 | 1 21
+8: 42 8 | 42
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+11: 42 31 # | 42 11 31
+12: 24 14 | 19 1
+13: 14 3 | 1 12
+14: "b"
+15: 1 | 14
+16: 15 1 | 14 14
+17: 14 2 | 1 7
+18: 15 15
+19: 14 1 | 14 14
+20: 14 14 | 1 15
+21: 14 1 | 1 14
+22: 14 14
+23: 25 1 | 22 14
+24: 14 1
+25: 1 1 | 1 14
+26: 14 22 | 1 20
+27: 1 6 | 14 18
+28: 16 1
+31: 14 17 | 1 13
+42: 9 14 | 10 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
 """
 
-s = """
-0: 2 1 3
-1: 2 2 | 3 3
-2: "a"
-3: "b"
-"""
 
-# aaab, abbb
+# s = """
+# 0: 4 1 5
+# 1: 2 3 | 3 2
+# 2: 4 4 | 5 5
+# 3: 4 5 | 5 4
+# 4: "a"
+# 5: "b"
+# """
+
+s = open("d19.txt").read()
 
 
-# '123': [('10', '20'), ('30', '40')]
-# '124': 'a'
 rules = {}
+inputs = []
 
 for line in s.strip().split("\n"):
-    m1 = re.match("(\d+): (.+)", line)
-    h = int(m1.group(1))  # head
-    t = m1.group(2)  # tail
-    if m2 := re.match('"(.)"', t):
-        rules[h] = m2.group(1)
+    line = line.strip()
+    if not line:
+        continue
+    if ":" in line:
+        m1 = re.match("(\\d+): ([^#]+)", line)
+        h = int(m1.group(1))  # head
+        t = m1.group(2)  # tail
+        if m2 := re.match('"(.)"', t):
+            rules[h] = m2.group(1)
+        else:
+            # set of tuples
+            rules[h] = tuple(
+                tuple(int(x) for x in c.split()) for c in t.split("|")
+            )  # c for clause
     else:
-        rules[h] = [[int(x) for x in c.split()] for c in t.split("|")]  # c for clause
-
-
-def parse_clause(seq, c):
-    """
-    c: list of heads
-    """
-
-    i = 0
-    matched = True
-    # All of these must match (AND)
-    for h in c:
-        print(">", h, parse_tail(seq[i:], rules[h]))
-    # for h in c:
-    #     b, d = parse_tail(seq[i:], rules[h])
-    #     if b:
-    #         i += d
-    #     else:
-    #         matched = False
-    #         break
-    # return matched
+        if line[0] != "#":
+            inputs.append(line)
 
 
 def parse_tail(seq, t):
@@ -58,37 +92,118 @@ def parse_tail(seq, t):
     t: char or list of clauses
     """
     if isinstance(t, str):
-        return seq[0] == t, 1
+        return t if seq[0] == t else None
     else:
         # One of these must match (OR)
         for c in t:
-            parse_clause(seq, c)
+            res = parse_clause(seq, c)
+            if res:
+                # print(c, res)
+                return res
+        return None
+
+
+def parse_clause(seq, c):
+    """
+    c: list of heads
+    """
+    # All of these must match (AND)
+    res = ""
+    for h in c:
+        new_res = parse_tail(seq[len(res) :], rules[h])
+        if new_res:
+            res += new_res
+        else:
+            res = None
             break
+    return res
 
 
-parse_tail("aaab", rules[0])
-
-# def generate(t, prefix=""):
-#     if isinstance(t, str):
-#         print(f"t: {t}")
-#         # return t
-#         words.add(prefix + t)
-#         return t
-#     else:
-#         for c in t:
-#             print(f"clause: {c}")
-#             # word = ""
-#             # h in [4, 1, 5]
-
-#             for h in c:
-#                 print(f"h: {h}")
-#                 res = generate(rules[h], prefix)
-#             # print(word)
+def is_valid(seq):
+    res = parse_tail(seq, rules[0])
+    return bool(res and len(res) == len(seq))
 
 
-# parse("ababbb", rules["0"])
+def generate(h):
+    def expand(x, h, c):
+        y = []
+        found = False
+        for hh in x:
+            if hh == h and not found:
+                y += list(c)
+                found = True
+            else:
+                y.append(hh)
+        return tuple(y)
 
-# 0: [4, 1, 5]
-# print(generate(rules["0"]))
+    out = rules[h]
 
-print()
+    while True:
+        out2 = []
+        for x in out:
+            found = False
+            for h in x:
+                t = rules[h]
+                if not isinstance(t, str):
+                    found = True
+                    break
+            if found:
+                for c in t:
+                    y = expand(x, h, c)
+                    out2.append(y)
+            else:
+                out2.append(x)
+        if out == out2:
+            break
+        # print(".", end="", flush=True)
+        out = out2
+
+    return {"".join(rules[h] for h in x) for x in out}
+
+
+# valid = generate(0)
+
+tokens_31 = generate(31)
+tokens_42 = generate(42)
+# tokens_27 = generate(27)
+# tokens_18 = generate(18)
+
+# print()
+# print(len(valid))
+#
+# print(sum(i in valid for i in inputs))
+
+# print()
+# for s in valid:
+#     print(s)
+
+# for s in valid:
+#     assert is_valid(s)
+
+
+def match_special(x):
+    tokens = {42: {"tokens": tokens_42, "n": 0}, 31: {"tokens": tokens_31, "n": 0}}
+    i = 0
+    for tid in (42, 31):
+        while True:
+            found = False
+            for t in tokens[tid]["tokens"]:
+                if t == x[i : i + len(t)]:
+                    found = True
+                    i += len(t)
+                    tokens[tid]["n"] += 1
+                    break
+            if not found:
+                break
+
+    return i == len(x) and tokens[31]["n"] > 0 and tokens[42]["n"] > tokens[31]["n"]
+
+
+# x = "babbbbaabbbbbabbbbbbaabaaabaaa"
+# print(match_special(x))
+# print(is_valid(x))
+# x = "babbbbaabbbbbbb"
+
+print(sum(match_special(i) for i in inputs))
+
+# match_special("aaabbbbbbaaaabaababaabababbabaaabbababababaaa")
